0a3677af010551558768ea72e5f77dc5230de869
joeymariano <joey.michalina.mariano@gmail.com>
2020-01-04 00:43:35 -0500

    download route reinstate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-  get '/*' do
+  get '/download' do
     if flash[:notice]
       signer = Aws::S3::Presigner.new
       mp3 = signer.presigned_url(:get_object, bucket: "cheapdinosaurs", key: "sicktunes/mp3/sicktunes_mp3.zip")
